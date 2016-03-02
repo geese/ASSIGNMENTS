c089f4e55714de8d1efbb497e573b022640ef23a
Geese <gisela.chodos@gmail.com>
2016-03-02 12:38:01 -0700

    final local query with annotations

--- Week7/A7.sql
+++ Week7/A7.sql
@@ -1,6 +1,26 @@
 USE CHODOS_TRAMS
 GO
 
+PRINT '';
+PRINT
+'1.	Writing user-defined function:
+		name:		dbo.GetLodgingTaxRate
+		parameters:	PropertyID, date
+		returns:	taxrate (as decimal) for PropertyID 
+					where taxtype = ''L'' (for Lodging Tax)
+					and date falls between start and end dates
+					of the found taxrate.
+		or returns:	0 for the taxrate if there is not a lodging
+					type taxrate for the property, or the date
+					is out of range.'
+PRINT '';
+
+GO
+
+
+
+
+
 IF EXISTS(SELECT name FROM sys.objects WHERE name = 'GetLodgingTaxRate')
 	DROP FUNCTION GetLodgingTaxRate;
 
@@ -36,9 +56,17 @@ AS
 			RETURN @result
 	END	
 
+
 GO
 
-SELECT dbo.GetLodgingTaxRate(10000,'3/1/16')
+PRINT '';
+PRINT
+'2.	Writing user-defined function:
+		name:		dbo.CalculateDeposit
+		parameters:	UnitRateID, CheckIn Date
+		returns:	(small money) Current Unit Rate * (1 + (dbo.GetLodgingTaxRate(PropertyID, CheckinDate) / 100) )
+		calls:		function #1 -- dbo.GetLodgingTaxRate()'
+PRINT '';
 
 GO
 
@@ -58,13 +86,52 @@ AS
 		DECLARE @PropID smallint = (SELECT PropertyID FROM UnitRate WHERE UnitRateID = @UnitRateID)
 
 		DECLARE @result smallmoney
-		SET @result = @CurrentRate * (1 + (.01 * dbo.GetLodgingTaxRate(@PropID,@CheckInDate)))
+		SET @result = @CurrentRate * (1 + (dbo.GetLodgingTaxRate(@PropID,@CheckInDate))/100)
 
 		RETURN @result
 	END
 GO
 
-SELECT dbo.CalculateDeposit(42,'3/1/16')
+
+
+
+PRINT '';
+PRINT
+'3.	Demonstrating the above functions:
+
+	3A:		Making new Reservation:			
+				ResDate = Today
+				ResStatus = "A"
+				Check-in Date = ''15 Aug 2015''
+				Nights = 3
+				Quoted Rate = dbo.CalculateDeposit
+				Deposit Paid = dbo.CalculateDeposit
+				CC Auth = ''3A Results''
+				Unit Rate ID = 13
+				Person ID = 7
+	3B:		Making new Reservation:				
+				ResDate = Today
+				ResStatus = "A"
+				Check-in Date = ''27 Aug 201''
+				Nights = 3
+				Quoted Rate = dbo.CalculateDeposit
+				Deposit Paid = dbo.CalculateDeposit
+				CC Auth = ''3B Results''
+				Unit Rate ID = 21
+				Person ID = 8
+	3C:		Making new Reservation:					
+				ResDate = Today
+				ResStatus = "A"
+				Check-in Date = ''25 Sep 2015''
+				Nights = 3
+				Quoted Rate = dbo.CalculateDeposit
+				Deposit Paid = dbo.CalculateDeposit
+				CC Auth = ''3C Results''
+				Unit Rate ID = 29
+				Person ID = 9'
+PRINT '';
+
+GO
 
 
 IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = 'sp_InsertReservation')
@@ -138,7 +205,33 @@ EXEC sp_InsertReservation
 	,	@PersonID			=	9
 
 
+GO
+PRINT ''
+PRINT ''
+PRINT 
+'3D:	Selecting * from Reservation to show the results:'
+PRINT '';
+
+GO
+
 SELECT * FROM Reservation
+
+GO
+
+PRINT ''
+PRINT
+'4A:	Writing user-defined function:
+			name:		dbo.CalculateCancellationFees
+			parameters:	reservationID, CancellationDate
+			returns:	TABLE with ReservationID, Original CheckIn Date, Deposit Paid, Cancellation Charge
+
+		Cancellation Policies:
+		Regardless of property or season, all cancellations made:
+			with more than 30 days’ notice are entitled to a 100% refund of the deposit paid (less a $25 administration fee).
+			14-30 days’ cancellation notice (75% refund, less a $25 administration fee)
+			8-13 days’ cancellation notice (50% refund, less a $25 administration fee)
+			7 days of less cancellation notice – No refund'
+PRINT ''
 
 GO
 
@@ -181,6 +274,13 @@ END
 
 GO
 
+PRINT ''
+PRINT
+'4B:	Demonstrating 4A -- dbo.CalculateCancellationFees() by passing in the reservations created in 3A, 3B, and 3C.
+		Cancellation date is 14 Aug 2015.'
+PRINT ''
+PRINT ''
+
 SELECT * 
 FROM dbo.CalculateCancellationFees(46, '14 Aug 2015')
 
@@ -197,6 +297,20 @@ dbo.CalculateCancellationFees(48, '14 Aug 2015')
 
 
 GO
+
+PRINT ''
+PRINT
+'5:		Writing user-defined function:
+			name:		dbo.fn_QuotedRate
+			parameters:	begin date, end date, PropertyID, UnitTypeID
+			returns:	TABLE @ValidRates
+						@ValidRates shows:	the maximum UnitRate applicable during any season/rate
+											that falls into the given date range,
+											and Unit Type Description
+											and additional field for Unit Rate Description
+											OR custom error message'
+PRINT ''
+
 
 IF EXISTS(SELECT name FROM sys.objects WHERE name = 'fn_QuotedRate')
 DROP FUNCTION  fn_QuotedRate
@@ -218,10 +332,12 @@ RETURNS @ValidRates	TABLE
 )
 AS
 	BEGIN
+		DECLARE @DEBUG_message varchar(70) = '';
 		IF DATEDIFF(day, @BeginDate, @EndDate) < 0
 			BEGIN
+			SET @DEBUG_message = CONCAT(CONVERT(char(12),@BeginDate,107), ' - ', CONVERT(char(12),@EndDate,107), ' is not a valid date range.')
 			INSERT INTO @ValidRates
-			VALUES(0, CONCAT(REPLICATE(' ',5),'----'), CONCAT(CONVERT(char(12),@BeginDate,107), ' - ', CONVERT(char(12),@EndDate,107), ' is not a valid date range.'))
+			VALUES(0, CONCAT(REPLICATE(' ',5),'----'), @DEBUG_message)
 			END
 		ELSE
 		BEGIN
@@ -258,6 +374,51 @@ AS
 
 GO
 
+PRINT ''
+PRINT 
+'5A:	Demonstrating dbo.fn_QuotedRate with parameters:
+			begin date:	1 July, 2015
+			end date:	30 Nov, 2015
+			propertyID:	10000
+			unitTypeID:	4'
+PRINT ''
+
+GO
+
 SELECT * FROM dbo.fn_QuotedRate('1 july, 2015', '30 NOV, 2015',10000,4) 
+
+
+GO
+
+PRINT ''
+PRINT ''
+PRINT 
+'5B:	Demonstrating dbo.fn_QuotedRate with parameters:
+			begin date:	1 July, 2015
+			end date:	30 Nov, 2015
+			propertyID:	11000
+			unitTypeID:	4'
+PRINT ''
+
+GO
+
+
 SELECT * FROM dbo.fn_QuotedRate('1 july, 2015', '30 NOV, 2015',11000,4)
+
+
+GO
+
+PRINT ''
+PRINT ''
+PRINT 
+'5C:	Demonstrating dbo.fn_QuotedRate with parameters:
+			begin date:	30 Nov, 2015
+			end date:	1 July, 2015	--BAD DATE RANGE
+			propertyID:	10000
+			unitTypeID:	4'
+PRINT ''
+
+GO
+
+
 SELECT * FROM dbo.fn_QuotedRate('30 NOV, 2015', '1 july, 2015',11000,4)  
